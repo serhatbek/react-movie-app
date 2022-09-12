d1ae60f9dd71243509e51dd45ac13740a1f7ca09
serhatbek <serhat.bekk@gmail.com>
2022-09-12 14:16:41 +0300

    show movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -6,7 +6,22 @@ const Movies = () => {
   console.log('home', movies);
   return (
     <section className='movies'>
-      <div className='container'>Movies</div>
+      <div className='container'>
+        {movies?.map((movie, index) => {
+          const { Title, Poster, Year, imdbID } = movie;
+
+          return (
+            <div className='movie-card' key={index}>
+              <figure>
+                <img src={Poster} alt={Title} />
+              </figure>
+              <h4>{Title}</h4>
+              <h5>{Year}</h5>
+              <span>{imdbID}</span>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
